dafb948859409d49b45ae67e1c97ebbeb7f22fdd
milesnardi <milesnardi@gmail.com>
2019-01-19 17:25:49 -0500

    added network request

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.makerequest = this.makerequest.bind(this);
+    this.state = {
+      close: 'Click Submit!'
+    }
+  }
+
+  makerequest(){
+    var params = {
+      apikey: "5NO4O6TFSS7HI49J",
+      symbol: "MSFT",
+      function: "GLOBAL_QUOTE"
+    };
+
+    var axiosInstance = axios.create({
+      baseURL: "https://www.alphavantage.co/query",
+      params: params
+    });
+    // Start a spinner
+    axiosInstance.get().then (response => {
+      console.log(JSON.stringify(response.data["Global Quote"]["08. previous close"]));
+      this.setState({
+        close: JSON.stringify(response.data["Global Quote"]["08. previous close"])
+      })
+      // stop the spinner
+    })
+  }
+
   render() {
+    var closeVariable = this.state.close
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello World.
           </p>
           <a
             className="App-link"
@@ -19,6 +50,8 @@ class App extends Component {
           >
             Learn React
           </a>
+          <button onClick={this.makerequest} >S u b m i t</button>
+          <p>MSFT: {closeVariable}</p>
         </header>
       </div>
     );
@@ -26,3 +59,12 @@ class App extends Component {
 }
 
 export default App;
+
+
+// git add * --- Adds files youwant to commit
+// git status --- check to see if all the files you want to commit is green
+// git commit -m "YOUR MESSAGE HERE, what did you change" --- add message to your commit
+// git push origin <branch> --- push to a particular branch. Master is the default
+
+// git checkout -b <unique_branch_name> -- creates a new branch
+// git branch --- see what branch you're in
